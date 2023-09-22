f3b341c91d98bf1c5d764083a4a9e51b4ed7a8e9
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-22 14:27:50 -0600

    updated about me

--- client/src/components/main/Home.js
+++ client/src/components/main/Home.js
@@ -7,8 +7,21 @@ function Home () {
             </div>
 
             <div class="flex-child">
-                <h3>A Little About Me</h3>
-                <p>Hello</p>
+                <div class="about-section">
+                    <h3>ABOUT ME</h3>
+                    <p>
+                    Hi there! I go by the name Tuyen and I was born in Vietnam but grew up in the United States. Nature has always held a special place in my heart, especially the beauty of sunrises and sunsets. In my free time, I enjoy hiking and capturing those breathtaking moments through photography.
+                        <br />
+                        <br />
+                        Becoming a mother at a young age presented its fair share of challenges, making it difficult for me to pursue a career that truly ignited my passion. Instead, I had to prioritize survival and raising my son as a single mother. I ended up working in the hospitality industry for over ten years, gaining valuable experience in customer service and management.
+                        <br />
+                        <br />
+                        However, last year, I made a life-changing decision. I seized the opportunity to become a stay-at-home mother so that I could finally pursue my dream career in Software Development. The world of Software Engineering had always fascinated me, but I doubted my abilities and hesitated to take the leap into a boot camp. After careful consideration, I realized that life is too short to hesitate, so I enrolled in Flatiron School and embarked on my journey as a Software Engineer.
+                        <br />
+                        <br />
+                        Today, I am incredibly proud to not only call myself a mother but also a driven Software Engineer.
+                    </p>
+                </div>
             </div>
         </div>
     )
