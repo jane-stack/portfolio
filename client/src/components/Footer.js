347dfc8158866e215b19a82970b0aeccc671108a
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-22 14:40:58 -0600

    fixed postioning of footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,15 +3,26 @@ function Footer () {
     return (
         <div className="footer-logos">
             <br/>
-            <a href="https://www.linkedin.com/in/stack-dev/" target="_blank" rel="noreferrer">
-                <img src="./Linkedin-logo-png.png" alt="linkedinlogo" height="15" />
-            </a>
-            <a href="https://github.com/jane-stack" target="_blank" rel="noreferrer">
-                <img src="./githublogo.png" alt="githublogo" height="15" /> 
-            </a>
-            <a href="https://dev.to/janestack" target="_blank" rel="noreferrer">
-                <img src="./3031814.avif" alt="githublogo" height="15" /> 
-            </a>
+            <div className="footer-div">
+                <a href="https://www.linkedin.com/in/stack-dev/" target="_blank" rel="noreferrer">
+                    <img src="./Linkedin-logo-png.png" alt="linkedinlogo" height="15" />
+                </a>
+            </div>
+
+            <br />
+            <div className="footer-div">
+                <a href="https://github.com/jane-stack" target="_blank" rel="noreferrer">
+                    <img src="./githublogo.png" alt="githublogo" height="15" /> 
+                </a>
+            </div>
+
+            <br />
+            <div className="footer-div">
+                <a href="https://dev.to/janestack" target="_blank" rel="noreferrer">
+                    {/* <img src="./3031814.avif" alt="githublogo" height="25" />  */}
+                    <strong>DEV</strong>
+                </a>
+            </div>
         </div>
     )
 }
