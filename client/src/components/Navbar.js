a67a8eb7c3f7924f51d83bf6697d238c765c997e
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-22 18:36:10 -0600

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,15 +6,15 @@ function Navbar () {
         <div className='navbar'>
             <div class="nav-container">
             <div class="nav-child">
-                <h1>Professional Portfolio</h1>
-                <h3>Full-Stack Engineer | Web Developer</h3>
+                <h1>TUYEN HA</h1>
+                <h3>FULL STACK DEVELOPER</h3>
                 <hr />
                 <Link className='nav-link' to="/">ABOUT ME</Link>
-                <Link className='nav-link' to="/portfolio">PORTFOLIO</Link>
+                <Link className='nav-link' to="/portfolio">PROJECTS</Link>
             </div>
             <div class="nav-child">
                 <img class="profile-pic" src="./IMG_2442.JPG" alt="profile-photo" width="100"/>
-                <h4>TUYEN HA</h4>
+                <h6>DENVER, COLORADO</h6>
             </div>
             </div>
         </div>
