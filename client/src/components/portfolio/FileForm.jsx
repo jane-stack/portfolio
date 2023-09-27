efdd17aa6de1b3d39a351f58b926bfe24827e184
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-26 21:00:04 -0600

    removed some server side features

--- client/src/components/portfolio/FileForm.jsx
+++ client/src/components/portfolio/FileForm.jsx
@@ -1,35 +1,35 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../../App';
+// import React, { useContext } from 'react'
+// import { AppContext } from '../../App';
 
 function FileForm() {
-    const { latestPost, setLatestPost } = useContext(AppContext);
+    // const { latestPost, setLatestPost } = useContext(AppContext);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const data = new FormData();
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     const data = new FormData();
 
-        data.append("project[title]", e.target.title.value);
-        data.append("project[description]", e.target.description.value);
-        data.append("project[image]", e.target.image.files[0]);
-        submitToAPI(data);
-    }
+    //     data.append("project[title]", e.target.title.value);
+    //     data.append("project[description]", e.target.description.value);
+    //     data.append("project[image]", e.target.image.files[0]);
+    //     submitToAPI(data);
+    // }
 
-    function submitToAPI(data) {
-        fetch("http://localhost:3000/projects", {
-            method: "POST",
-            body: data
-        })
-        .then(resp => resp.json())
-        .then(data => {
-            setLatestPost(data.image_url);
-        })
-        .catch((error) => console.log(error));
-    }
+    // function submitToAPI(data) {
+    //     fetch("http://localhost:3000/projects", {
+    //         method: "POST",
+    //         body: data
+    //     })
+    //     .then(resp => resp.json())
+    //     .then(data => {
+    //         setLatestPost(data.image_url);
+    //     })
+    //     .catch((error) => console.log(error));
+    // }
 
   return (
     <div>
         <h3>UPLOAD PROJECTS</h3>
-        <form className="upload-form" onSubmit={(e) => handleSubmit}>
+        <form className="upload-form">
             {/* <label htmlFor='title'>Title</label> */}
             <input className='input' type='text' name='title' id='title' placeholder='TITLE' />
             <br />
--- client/src/components/portfolio/LastestImage.jsx
+++ client/src/components/portfolio/LastestImage.jsx
@@ -1,21 +1,21 @@
-import React, { useContext, useEffect } from 'react'
-import { AppContext } from '../../App'
+// import React, { useContext, useEffect } from 'react'
+// import { AppContext } from '../../App'
 
 function LastestImage() {
-    const { latestPost, setLatestPost } = useContext(AppContext);
+    // const { latestPost, setLatestPost } = useContext(AppContext);
 
-    useEffect(() => {
-        fetch("http://localhost:3000/latest")
-            .then(resp => resp.json())
-            .then(data => {
-                setLatestPost(data.image_url);
-            })
-            .catch((error) => console.log(error));
-    }, [latestPost]);
+    // useEffect(() => {
+    //     fetch("http://localhost:3000/latest")
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             setLatestPost(data.image_url);
+    //         })
+    //         .catch((error) => console.log(error));
+    // }, [latestPost]);
 
   return (
     <div>
-        <img src={latestPost} alt="latest post" className='latest-image' />
+        {/* <img src={latestPost} alt="latest post" className='latest-image' /> */}
     </div>
   )
 }
