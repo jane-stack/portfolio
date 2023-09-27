7ab6463dbd73f67c062ad1d7448aaa005683bb4e
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-26 21:43:59 -0600

    added projects

--- client/src/components/portfolio/PortfolioPage.js
+++ client/src/components/portfolio/PortfolioPage.js
@@ -8,6 +8,14 @@ function PortfolioPage () {
             <div className="upload-project">
                 {/* <FileForm />
                 <LastestImage /> */}
+                <div className="project-img">
+                    <h4>Social Media Demo</h4>
+                    <img src="socialmedia.png" alt="project1" width="200"/>
+                </div>
+                <div className="project-img">
+                    <h4>Rapi Car Rental</h4>
+                    <img src="rapirental.png" alt="project2" width="200"/>
+                </div>
             </div>
         </div>
     )
